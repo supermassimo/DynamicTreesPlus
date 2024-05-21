9639cfaab3d25153e4f8a473605c7001ea41adbd
BuildTools <unconfigured@null.spigotmc.org>
2024-05-21 17:09:22 -0400

    - Updated ModelBakery to ModelBaker

--- src/main/java/com/ferreusveritas/dynamictreesplus/model/geometry/CactusBranchBlockModelGeometry.java
+++ src/main/java/com/ferreusveritas/dynamictreesplus/model/geometry/CactusBranchBlockModelGeometry.java
@@ -6,7 +6,7 @@ import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.Material;
-import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelBaker;
 import net.minecraft.client.resources.model.ModelState;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
@@ -27,7 +27,7 @@ public class CactusBranchBlockModelGeometry extends BranchBlockModelGeometry {
 
 
     @Override
-    public BakedModel bake(IGeometryBakingContext owner, ModelBakery bakery, Function<Material, TextureAtlasSprite> spriteGetter, ModelState modelTransform, ItemOverrides overrides, ResourceLocation modelLocation) {
+    public BakedModel bake(IGeometryBakingContext owner, ModelBaker baker, Function<Material, TextureAtlasSprite> spriteGetter, ModelState modelTransform, ItemOverrides overrides, ResourceLocation modelLocation) {
         return new CactusBranchBlockBakedModel(modelLocation, this.barkTextureLocation, this.ringsTextureLocation, spriteGetter);
     }
 
